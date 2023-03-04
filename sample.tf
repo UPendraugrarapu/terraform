9ee2726676074dc917b1965e55310562438ee4e3
upendra <ugrarapuupendra@gmail.com>
2023-03-04 12:50:32 +0530

    update

--- sample.tf
+++ sample.tf
@@ -1,8 +1,7 @@
 resource "aws_instance" "ec2" {
-
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
-    vpc_security_group_ids = "sg-0901684fec359fab8"
+    vpc_security_group_ids = ["sg-0901684fec359fab8"]
 
     tags = {
       "name" = "sample"
