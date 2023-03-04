1b74ce1a278f86d84311ba31017ce7cbc8c00dcc
upendra <ugrarapuupendra@gmail.com>
2023-03-04 17:58:22 +0530

    route53 code update

--- roboshop-setup/ex1/server.tf
+++ roboshop-setup/ex1/server.tf
@@ -5,9 +5,17 @@ resource "aws_instance" "frontend" {
 
     tags = {
       Name = "frontend"
-    }
-  
+       }
+
 }
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z10169603GCPS6PZET2FX"
+  name    = "frontend-dev.devopsb71.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "cart" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
@@ -18,6 +26,14 @@ resource "aws_instance" "cart" {
     }
   
 }
+resource "aws_route53_record" "cart" {
+  zone_id = "Z10169603GCPS6PZET2FX"
+  name    = "cart-dev.devopsb71.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
+
 resource "aws_instance" "mongodb" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
@@ -28,6 +44,14 @@ resource "aws_instance" "mongodb" {
     }
   
 }
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z10169603GCPS6PZET2FX"
+  name    = "mongodb-dev.devopsb71.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
+
 resource "aws_instance" "catalogue" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
@@ -38,6 +62,14 @@ resource "aws_instance" "catalogue" {
     }
   
 }
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z10169603GCPS6PZET2FX"
+  name    = "catalogue-dev.devopsb71.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
+
 resource "aws_instance" "user" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
@@ -48,6 +80,14 @@ resource "aws_instance" "user" {
     }
   
 }
+resource "aws_route53_record" "user" {
+  zone_id = "Z10169603GCPS6PZET2FX"
+  name    = "user-dev.devopsb71.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
+
 resource "aws_instance" "shipping" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
@@ -58,6 +98,14 @@ resource "aws_instance" "shipping" {
     }
   
 }
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z10169603GCPS6PZET2FX"
+  name    = "shipping-dev.devopsb71.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
+
 resource "aws_instance" "redis" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
@@ -68,6 +116,14 @@ resource "aws_instance" "redis" {
     }
   
 }
+resource "aws_route53_record" "redis" {
+  zone_id = "Z10169603GCPS6PZET2FX"
+  name    = "redi-dev.devopsb71.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
+}
+
 resource "aws_instance" "payment" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
@@ -78,6 +134,14 @@ resource "aws_instance" "payment" {
     }
   
 }
+resource "aws_route53_record" "payment" {
+  zone_id = "Z10169603GCPS6PZET2FX"
+  name    = "payment-dev.devopsb71.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
+}
+
 resource "aws_instance" "rabbitmq" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
@@ -88,6 +152,14 @@ resource "aws_instance" "rabbitmq" {
     }
   
 }
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z10169603GCPS6PZET2FX"
+  name    = "rabbitmq-dev.devopsb71.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
 resource "aws_instance" "mysql" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
@@ -98,6 +170,14 @@ resource "aws_instance" "mysql" {
     }
   
 }
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z10169603GCPS6PZET2FX"
+  name    = "mysql-dev.devopsb71.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
+
 resource "aws_instance" "dispatch" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
@@ -107,4 +187,11 @@ resource "aws_instance" "dispatch" {
       Name = "dispatch"
     }
   
+}
+resource "aws_route53_record" "dispatch" {
+  zone_id = "Z10169603GCPS6PZET2FX"
+  name    = "dispatch-dev.devopsb71.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.dispatch.private_ip]
 }
