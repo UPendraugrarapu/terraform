f598529385ae626b51747252f93c859be6768826
upendra <ugrarapuupendra@gmail.com>
2023-03-06 15:28:43 +0530

    update provisioner

---
+++ 7.provisioners/main.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "ec2" {
+    ami = "ami-0a017d8ceb274537d"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = ["sg-0901684fec359fab8"]
+
+    tags = {
+      Name = "demo"
+    }
+  
+
+#remote-exec-in remote machine
+#local-exec- in local machine
+   provisioner "remote-exec" {
+    host = self.public_ip
+    user = "root"
+    password = "DevOps321"
+   }
+
+   inline = [
+    "echo Hi"
+   ]
+
+   }
